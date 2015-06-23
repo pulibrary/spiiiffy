2e9103d1f89c1beefe9b7e36841ad30a3e1337c4
Shaun Ellis <shaun@sdellis.com>
2015-06-23 11:49:44 -0400

    adding capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,51 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'spiiiffy'
+set :repo_url, 'git@github.com:pulibrary/spiiiffy.git'
+set :branch, 'master'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/opt/spiiiffy'
+
+# Default value for :scm is :git
+set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+shared_path = "#{:deploy_to}/shared"
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/initializers/secret_token.rb')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'db/seeds')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
