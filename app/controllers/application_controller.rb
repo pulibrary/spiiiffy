a6ab76e094c07148e791ec4b02237a709af8a65f
Shaun Ellis <shaun@sdellis.com>
2015-07-09 11:18:22 -0400

    fixing base url in dev so asset paths work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def default_url_options
+    if Rails.env.production?
+      {:host => "libruby-dev.princeton.edu/spiiiffy"}
+    else
+      {}
+    end
+  end
   #before_action :authenticate_user!
 
 end
